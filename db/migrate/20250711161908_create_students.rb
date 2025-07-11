664a959421d2fc611f373062e2605f94c53fd596
adamhannadev <adamhannadev@gmail.com>
2025-07-11 09:19:27 -0700

    Add student model

---
+++ db/migrate/20250711161908_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :students do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
